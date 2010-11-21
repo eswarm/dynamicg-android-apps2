06d3a7a23e4ea8eb5085c9116e5d6cfd67503f19
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-11-21 09:17:07 +0000

    add prefsBean

--- branches/BookmarkTree.1.06.b/src/com/dynamicg/bookmarkTree/PreferencesBean.java
+++ branches/BookmarkTree.1.06.b/src/com/dynamicg/bookmarkTree/PreferencesBean.java
@@ -2,13 +2,14 @@ package com.dynamicg.bookmarkTree;
 
 public class PreferencesBean {
 
-	public String folderSeparator;
-	public String nodeConcatenation;
-	public int disclaimerLastDisplayed;
-	public int showDeleteIcon;
-	public int optimisedLayout;
-	public int listStyle;
-	public int sortOption;
+	protected String folderSeparator;
+	protected String nodeConcatenation;
+	protected int disclaimerLastDisplayed;
+	protected int showDeleteIcon;
+	protected int optimisedLayout;
+	protected int listStyle;
+	protected int sortOption;
+	
 	public String getFolderSeparator() {
 		return folderSeparator;
 	}
