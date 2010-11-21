956e3bccfd97e9e4caa41f871e2b77250a64c8b1
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-11-21 09:18:34 +0000

    add prefsBean

--- branches/BookmarkTree.1.06.b/src/com/dynamicg/bookmarkTree/PreferencesBean.java
+++ branches/BookmarkTree.1.06.b/src/com/dynamicg/bookmarkTree/PreferencesBean.java
@@ -13,44 +13,17 @@ public class PreferencesBean {
 	public String getFolderSeparator() {
 		return folderSeparator;
 	}
-	public void setFolderSeparator(String folderSeparator) {
-		this.folderSeparator = folderSeparator;
-	}
 	public String getNodeConcatenation() {
 		return nodeConcatenation;
-	}
-	public void setNodeConcatenation(String nodeConcatenation) {
-		this.nodeConcatenation = nodeConcatenation;
 	}
 	public int getDisclaimerLastDisplayed() {
 		return disclaimerLastDisplayed;
 	}
-	public void setDisclaimerLastDisplayed(int disclaimerLastDisplayed) {
-		this.disclaimerLastDisplayed = disclaimerLastDisplayed;
-	}
-	public int getShowDeleteIcon() {
-		return showDeleteIcon;
-	}
-	public void setShowDeleteIcon(int showDeleteIcon) {
-		this.showDeleteIcon = showDeleteIcon;
-	}
-	public int getOptimisedLayout() {
-		return optimisedLayout;
-	}
-	public void setOptimisedLayout(int optimisedLayout) {
-		this.optimisedLayout = optimisedLayout;
-	}
 	public int getListStyle() {
 		return listStyle;
 	}
-	public void setListStyle(int listStyle) {
-		this.listStyle = listStyle;
-	}
 	public int getSortOption() {
 		return sortOption;
-	}
-	public void setSortOption(int sortOption) {
-		this.sortOption = sortOption;
 	}
 	
 }
