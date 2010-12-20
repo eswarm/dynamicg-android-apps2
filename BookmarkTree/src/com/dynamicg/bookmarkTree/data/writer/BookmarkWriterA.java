ba6f91d6e2c3222de18d52588e5bfec021a2b24a
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-20 17:33:34 +0000

    cleanup

--- BookmarkTree/src/com/dynamicg/bookmarkTree/data/writer/BookmarkWriterA.java
+++ BookmarkTree/src/com/dynamicg/bookmarkTree/data/writer/BookmarkWriterA.java
@@ -3,8 +3,6 @@ package com.dynamicg.bookmarkTree.data.writer;
 import java.util.ArrayList;
 
 import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.provider.Browser;
 
 import com.dynamicg.bookmarkTree.BookmarkTreeContext;
 import com.dynamicg.bookmarkTree.data.BrowserBookmarkLoader;
@@ -19,21 +17,9 @@ public abstract class BookmarkWriterA {
 		this.ctx = ctx;
 		contenResolver = ctx.activity.getContentResolver(); 
 	}
-
-	public void updateBookmarkTitle(Integer bookmarkId, String newTitle) {
-		ContentValues values = new ContentValues();
-		values.put(Browser.BookmarkColumns.TITLE, newTitle.toString());
-		contenResolver.update ( Browser.BOOKMARKS_URI
-				, values
-				, Browser.BookmarkColumns._ID+"=?"
-				, new String[]{Integer.toString(bookmarkId)}
-		);
-	}
-
+	
 	public static ArrayList<BrowserBookmarkBean> getBrowserBookmarks(BookmarkTreeContext ctx) {
-		return BrowserBookmarkLoader.loadBrowserBookmarks(ctx.activity);
+		 return BrowserBookmarkLoader.loadBrowserBookmarks(ctx.activity);
 	}
-
-
-
+	
 }
