4ec362db5e38ac6217473331e0f2a069cf3f5e63
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-20 16:57:21 +0000

    common popup dialog

--- BookmarkTree/src/com/dynamicg/bookmarkTree/util/DialogButtonPanelWrapper.java
+++ BookmarkTree/src/com/dynamicg/bookmarkTree/util/DialogButtonPanelWrapper.java
@@ -4,14 +4,21 @@ import android.app.Dialog;
 import android.view.View;
 import android.widget.Button;
 
+import com.dynamicg.bookmarkTree.R;
+
 public abstract class DialogButtonPanelWrapper {
 
 	private final Dialog dialog;
 
-	public DialogButtonPanelWrapper(final Dialog dialog
+	public DialogButtonPanelWrapper(final Dialog dialog) {
+		this(dialog, R.id.buttonOk, R.id.buttonCancel);
+	}
+	
+	private DialogButtonPanelWrapper(final Dialog dialog
 			, int residPositiveButton
 			, int residNegativeButton
-			) {
+			) 
+	{
 		this.dialog = dialog;
 
 		Button positiveButton = (Button)dialog.findViewById(residPositiveButton);
