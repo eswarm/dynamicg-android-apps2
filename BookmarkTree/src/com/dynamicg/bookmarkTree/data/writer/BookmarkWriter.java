ae0e7b319bf5625f78df0d51c74e7da139490b71
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-21 15:40:43 +0000

    consolidate BookmarkWriter

--- BookmarkTree/src/com/dynamicg/bookmarkTree/data/writer/BookmarkWriter.java
+++ BookmarkTree/src/com/dynamicg/bookmarkTree/data/writer/BookmarkWriter.java
@@ -4,17 +4,20 @@ import android.content.ContentValues;
 import android.provider.Browser;
 
 import com.dynamicg.bookmarkTree.BookmarkTreeContext;
+import com.dynamicg.common.main.Logger;
 
-public class BookmarkUpdateWriter extends BookmarkWriterA {
+public class BookmarkWriter extends BookmarkWriterA {
 
+	private static final Logger log = new Logger(BookmarkWriter.class);
+	
 	private final ContentValues values;
 
-	public BookmarkUpdateWriter(BookmarkTreeContext ctx) {
+	public BookmarkWriter(BookmarkTreeContext ctx) {
 		super(ctx);
 		values = new ContentValues();
 	}
 
-	private void write(Integer bookmarkId) {
+	private void doUpdate(Integer bookmarkId) {
 		contenResolver.update ( Browser.BOOKMARKS_URI
 				, values
 				, Browser.BookmarkColumns._ID+"=?"
@@ -24,13 +27,23 @@ public class BookmarkUpdateWriter extends BookmarkWriterA {
 	
 	public void updateTitle(Integer bookmarkId, String title) {
 		values.put(Browser.BookmarkColumns.TITLE, title);
-		write(bookmarkId);
+		doUpdate(bookmarkId);
 	}
 
 	public void updateTitleAndUrl(Integer bookmarkId, String title, String url) {
 		values.put(Browser.BookmarkColumns.TITLE, title);
 		values.put(Browser.BookmarkColumns.URL, url);
-		write(bookmarkId);
+		doUpdate(bookmarkId);
+	}
+	
+	public void deleteBrowserBookmark(Integer id) {
+		if (log.isDebugEnabled()) {
+			log.debug("delete bookmark", id);
+		}
+		contenResolver.delete ( Browser.BOOKMARKS_URI
+				, Browser.BookmarkColumns._ID+"=?"
+				, new String[]{Integer.toString(id)}
+				);
 	}
 	
 }
