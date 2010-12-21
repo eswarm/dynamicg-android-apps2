c82d8cbefb41f08f042cc1090ff60c94521ddad1
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-21 15:39:23 +0000

    i18n strings

--- trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/util/DialogButtonPanelWrapper.java
+++ trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/util/DialogButtonPanelWrapper.java
@@ -11,7 +11,7 @@ public abstract class DialogButtonPanelWrapper {
 	private final Dialog dialog;
 
 	public DialogButtonPanelWrapper(final Dialog dialog) {
-		this(dialog, R.id.buttonOk, R.id.buttonCancel);
+		this(dialog, R.id.buttonSave, R.id.buttonCancel);
 	}
 	
 	private DialogButtonPanelWrapper(final Dialog dialog
