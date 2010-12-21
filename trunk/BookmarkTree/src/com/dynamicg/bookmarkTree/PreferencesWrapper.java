1c45cd61a97505c467200526aa764dfd598c7831
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-21 16:07:36 +0000

    naming

--- trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/PreferencesWrapper.java
+++ trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/PreferencesWrapper.java
@@ -93,8 +93,8 @@ public class PreferencesWrapper {
 	}
 	
 	public static final int SORT_ALPHA = 0;
-	public static final int SORT_FOLDERS_BEFORE_BM = 1;
-	public static final int SORT_BM_BEFORE_FOLDERS = 2;
+	public static final int SORT_FOLDERS_FIRST = 1;
+	public static final int SORT_BOOKMARKS_FIRST = 2;
 	
 	public static final int LIST_STYLE_CLASSIC = 0;
 	public static final int LIST_STYLE_COMPACT = 1;
