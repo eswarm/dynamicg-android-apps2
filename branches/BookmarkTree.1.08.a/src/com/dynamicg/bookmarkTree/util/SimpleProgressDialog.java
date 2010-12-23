6f6a704e155dbd0bff70af4ac1a61c5809940bda
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-23 15:12:19 +0000

    catch errors in progress

--- branches/BookmarkTree.1.08.a/src/com/dynamicg/bookmarkTree/util/SimpleProgressDialog.java
+++ branches/BookmarkTree.1.08.a/src/com/dynamicg/bookmarkTree/util/SimpleProgressDialog.java
@@ -5,10 +5,17 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 
+import com.dynamicg.bookmarkTree.R;
+import com.dynamicg.common.main.SystemUtil;
+import com.dynamicg.common.ui.SimpleAlertDialog;
+
 public abstract class SimpleProgressDialog {
+
+	private final Context context;
 
 	public SimpleProgressDialog(Context context, String title) {
 		
+		this.context = context;
 		final ProgressDialog progressDialog;
 		progressDialog = new ProgressDialog(context);
 		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
@@ -18,15 +25,29 @@ public abstract class SimpleProgressDialog {
 
 		final Handler doneHandler = new Handler() {
 			public void handleMessage(Message msg) {
-				done();
-				progressDialog.dismiss();
+				if (msg.what==0) {
+					done();
+					progressDialog.dismiss();
+				}
+				else if (msg.what==1) {
+					progressDialog.dismiss();
+					handleError ( (Throwable)msg.obj );
+				}
 			}
 		};
 
 		Thread progressThread = new Thread(new Runnable() {
 			public void run() {
-				backgroundWork();
-				doneHandler.sendEmptyMessage(0);
+				try {
+					backgroundWork();
+					doneHandler.sendEmptyMessage(0);
+				}
+				catch (Throwable e) {
+					Message msg = new Message();
+					msg.what=1;
+					msg.obj=e;
+					doneHandler.sendMessage(msg);
+				}
 			}
 		}) ;
 
@@ -36,5 +57,22 @@ public abstract class SimpleProgressDialog {
 	
 	public abstract void backgroundWork();
 	public abstract void done();
+	
+	/*
+	 * override in implementations if required
+	 */
+	public void handleError(final Throwable e) {
+		
+		new SimpleAlertDialog(context, "Error", R.string.commonClose) {
+			@Override
+			public String getScrollViewText() {
+				return SystemUtil.getExceptionText(e);
+			}
+		};
+		
+		if (SystemUtil.isDevelopmentOrDevDevice()) {
+			e.printStackTrace(System.err);
+		}
+	}
 	
 }
