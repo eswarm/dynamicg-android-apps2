f413c023e6c9b63bb9724ec6203ed254c2dc21d8
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-23 18:58:10 +0000

    msg

--- branches/BookmarkTree.1.08.a/src/com/dynamicg/bookmarkTree/backup/Messages.java
+++ branches/BookmarkTree.1.08.a/src/com/dynamicg/bookmarkTree/backup/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
 	public static final String brProgressCreateBackup = "Create backup ...";
 	public static final String brHintBackupCreated = "Backup created:\n{1}";
 	public static final String brProgressRestoreBookmarks = "Restore bookmarks ...";
-	public static final String brHintBookmarksRestored = "Restore done ...";
+	public static final String brHintBookmarksRestored = "Restore done";
 	public static final String brNoFilesForRestore = "\u2192 No backup files available";
 	public static final String  brStorageHint = "\u2192 Backup files are stored here:";
 	
