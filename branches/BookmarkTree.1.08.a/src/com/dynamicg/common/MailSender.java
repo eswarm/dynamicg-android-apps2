f8e0c3829956be2db8143760ca5317d9ec121809
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-24 18:12:56 +0000

    add "email to dev"

--- branches/BookmarkTree.1.08.a/src/com/dynamicg/common/MailSender.java
+++ branches/BookmarkTree.1.08.a/src/com/dynamicg/common/MailSender.java
@@ -9,26 +9,19 @@ import com.dynamicg.bookmarkTree.ui.AboutDialog;
 
 public class MailSender {
 
-	private static void prepareBasicSendMailIntent ( Context context, String title, String body) {
-		
-		final String targetIntent;
-		targetIntent = Intent.ACTION_SEND;
-		Intent msg = new Intent(targetIntent);
-		
+	private static void createIntent ( Context context, String title, String body) {
+		Intent msg = new Intent(Intent.ACTION_SEND);
+		msg.setType("text/plain");
 		msg.putExtra(Intent.EXTRA_SUBJECT, title);
 		msg.putExtra(Intent.EXTRA_TEXT, body);
-		
-		// email recipient
-		String emailRecipient = AboutDialog.AUTHOR;
-		msg.putExtra(Intent.EXTRA_EMAIL, new String[]{emailRecipient} );
-		
-		context.startActivity(Intent.createChooser(msg, title));
+		msg.putExtra(Intent.EXTRA_EMAIL, new String[]{AboutDialog.AUTHOR} );
+		context.startActivity(Intent.createChooser(msg, "Send error report"));
 	}
 	
 	public static void emailError(Context context, Throwable exception) {
 		String title = "Bookmark Tree Manager ("+Locale.getDefault().getLanguage()+")";
 		String body = SystemUtil.getExceptionText(exception);
-		prepareBasicSendMailIntent(context, title, body);
+		createIntent(context, title, body);
 	}
 	
 }
