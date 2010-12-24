01da75eff52de46787b61071d8e4da9af90be2bc
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2010-12-24 21:07:21 +0000

    sd card check

--- branches/BookmarkTree.1.08.a/src/com/dynamicg/bookmarkTree/backup/SDCardCheck.java
+++ branches/BookmarkTree.1.08.a/src/com/dynamicg/bookmarkTree/backup/SDCardCheck.java
@@ -80,7 +80,7 @@ public class SDCardCheck {
 		}
 		
 		if ( !Environment.MEDIA_MOUNTED.equals(sdCardState)
-				|| !Environment.MEDIA_MOUNTED_READ_ONLY.equals(sdCardState) )
+				&& !Environment.MEDIA_MOUNTED_READ_ONLY.equals(sdCardState) )
 		{
 			alert(context, errorTitle, "SD Card is not available.\nCurrent state is '"+sdCardState+"'");
 		}
