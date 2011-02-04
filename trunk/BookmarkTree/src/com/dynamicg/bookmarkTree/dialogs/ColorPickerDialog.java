a6ea21fad8abc98847f6856ad41a104110d949e0
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2011-02-04 21:40:33 +0000

    button panel

--- trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/dialogs/ColorPickerDialog.java
+++ trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/dialogs/ColorPickerDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
@@ -21,6 +22,7 @@ import com.dynamicg.common.ContextUtil;
 public class ColorPickerDialog extends Dialog {
 
 	private static final int TEXTSIZE = 22;
+	private static final int BUTTON_WIDTH = 110;
 	
 	private final Context context;
 	private final ColorSelectedListener colorSelectedListener;
@@ -89,23 +91,9 @@ public class ColorPickerDialog extends Dialog {
 		layout.addView ( createBar(colorPickerGreen, R.drawable.progress_bg_green) );
 		layout.addView ( createBar(colorPickerBlue, R.drawable.progress_bg_blue) );
 		
-		Button btSave = new Button(context);
-		btSave.setText(R.string.commonOK);
-		btSave.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (colorSelectedListener!=null) {
-					colorSelectedListener.colorSelected(getSelectedColor());
-				}
-				else {
-					System.err.println("COLOR SELECTED:"+getSelectedColor());
-				}
-				dismiss();
-			}
-		});
-		layout.addView(btSave);
-		
 		updateTitleColor();
+		
+		addButtonPanel(layout);
 		
 		ScrollView scrollView = new ScrollView(context);
 		scrollView.addView(layout);
@@ -118,6 +106,51 @@ public class ColorPickerDialog extends Dialog {
 	
 	private void maximizeWindowWidth() {
 		getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+	}
+	
+	private void addButtonPanel(LinearLayout parent) {
+		
+		int scaledButtonWidth = ContextUtil.getScaledSizeInt(context, BUTTON_WIDTH);
+		LinearLayout panel = new LinearLayout(context);
+		panel.setOrientation(LinearLayout.HORIZONTAL);
+		panel.setGravity(Gravity.CENTER_HORIZONTAL);
+		
+		{
+			Button btSave = new Button(context);
+			btSave.setText(R.string.commonOK);
+			btSave.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					if (colorSelectedListener!=null) {
+						colorSelectedListener.colorSelected(getSelectedColor());
+					}
+					else {
+						// for debugging
+						System.err.println("COLOR SELECTED:"+getSelectedColor());
+					}
+					dismiss();
+				}
+			});
+			btSave.setWidth(scaledButtonWidth);
+			
+			panel.addView(btSave);
+		}
+		
+		{
+			Button btCancel = new Button(context);
+			btCancel.setText(R.string.commonCancel);
+			btCancel.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					dismiss();
+				}
+			});
+			btCancel.setWidth(scaledButtonWidth);
+			panel.addView(btCancel);
+		}
+		
+		parent.addView(panel);
+		
 	}
 	
 	private int getSelectedColor() {
