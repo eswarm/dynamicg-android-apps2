65d957b211a633787d609cc20776928f9ef0c217
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2011-02-06 13:30:26 +0000

    increase minsdk to 2.0

--- branches/BookmarkTree.1.13.a/src/com/dynamicg/bookmarkTree/bitmapScaler/BitmapScaleManager.java
+++ branches/BookmarkTree.1.13.a/src/com/dynamicg/bookmarkTree/bitmapScaler/BitmapScaleManager.java
@@ -5,33 +5,9 @@ import android.graphics.BitmapFactory;
 
 import com.dynamicg.bookmarkTree.prefs.PreferencesWrapper;
 
-/*
- * Bitmap.setDensity() is API4 so we need to wrap
- */
 public class BitmapScaleManager {
 
-	public static interface ScaleWorker {
-		public final int DFLT_DENSITY = 160;
-		public Bitmap scaleForList(Bitmap b); // returns 'b' for chaining
-		public void setDensity(Bitmap b, int density);
-		public int getDensity(Bitmap b);
-	}
-	
-	private static final ScaleWorker scaleWorker;
-	private static final boolean api3;
-	
-	static {
-		ScaleWorker localScaleWorker;
-		try {
-			Class<?> scaleWorkerClass = Class.forName("com.dynamicg.bookmarkTree.bitmapScaler.BitmapScalerAPI4");
-			localScaleWorker = (ScaleWorker)scaleWorkerClass.newInstance();
-		}
-		catch (Throwable e) {
-			localScaleWorker = new BitmapScalerAPI3();
-		}
-		scaleWorker = localScaleWorker;
-		api3 = localScaleWorker instanceof BitmapScalerAPI3;
-	}
+	public static final int DFLT_DENSITY = 160;
 	
 	private static boolean enabledForList; 
 	
@@ -50,20 +26,10 @@ public class BitmapScaleManager {
 			// error report Dec 28, 2010 8:35:05 PM
 			return null;
 		}
-		if (enabledForList && !api3) {
-			return scaleWorker.scaleForList(b);
+		if (enabledForList) {
+			b.setDensity(DFLT_DENSITY);
 		}
 		return b;
-	}
-	
-	// for shortcut creation
-	public static void scale(Bitmap b, int density) {
-		scaleWorker.setDensity(b, density);
-	}
-	
-	// for shortcut creation
-	public static int getDensity(Bitmap b) {
-		return scaleWorker.getDensity(b);
 	}
 	
 }
