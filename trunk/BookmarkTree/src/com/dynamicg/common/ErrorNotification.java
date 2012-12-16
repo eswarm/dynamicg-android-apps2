af94e149126fe3911e10cee1b7e43810c20d2831
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2012-12-16 20:50:28 +0000

    rename app to "Bookmark Folders"

--- trunk/BookmarkTree/src/com/dynamicg/common/ErrorNotification.java
+++ trunk/BookmarkTree/src/com/dynamicg/common/ErrorNotification.java
@@ -18,30 +18,30 @@ public class ErrorNotification {
 		msg.putExtra(Intent.EXTRA_EMAIL, new String[]{AboutDialog.AUTHOR} );
 		context.startActivity(Intent.createChooser(msg, "Send error report"));
 	}
-	
+
 	private static void emailError(Context context, String alertTitle, Throwable exception) {
 		String version = ContextUtil.getVersion(context)[0];
-		String title = "Bookmark Tree "+version+" - Error ("+Locale.getDefault().getLanguage()+")";
+		String title = context.getString(R.string.app_name)+" "+version+" Error ("+Locale.getDefault().getLanguage()+")";
 		String body = alertTitle+"\n\n"+SystemUtil.getExceptionText(exception);
 		createIntent(context, title, body);
 	}
-	
+
 	public static void notifyError(final Context context, final String title, final Throwable e) {
 		final String alertTitle =  "Error: "+title;
 		new SimpleAlertDialog ( context, alertTitle, "Email DEV", context.getString(R.string.commonClose)) {
-			
+
 			@Override
 			public String getScrollViewText() {
 				return SystemUtil.getExceptionText(e);
 			}
-	
+
 			@Override
 			public void onPositiveButton() {
 				emailError(context, alertTitle, e);
 			}
-			
+
 		};
 		Logger.dumpIfDevelopment(e);
 	}
-	
+
 }
