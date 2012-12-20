3e6a8d0f9e03aaa37ff7623c2738509be5b8f410
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2012-12-20 16:37:36 +0000

    make prefs reloadable

--- BookmarkTree/src/com/dynamicg/bookmarkTree/prefs/PrefEntryInt.java
+++ BookmarkTree/src/com/dynamicg/bookmarkTree/prefs/PrefEntryInt.java
@@ -7,17 +7,19 @@ import com.dynamicg.bookmarkTree.BookmarkTreeContext;
 public class PrefEntryInt {
 
 	protected static ArrayList<PrefEntryInt> cache = new ArrayList<PrefEntryInt>();
-	
+
 	public final String name;
+	private final int defValue;
 	public int value;
 	public int updatedValue;
-	
+
 	public PrefEntryInt(String name, int defValue) {
 		this.name = name;
+		this.defValue = defValue;
 		this.value = BookmarkTreeContext.settings.getInt(name, defValue);
 		cache.add(this);
 	}
-	
+
 	public boolean isOn() {
 		return value==1;
 	}
@@ -31,11 +33,15 @@ public class PrefEntryInt {
 			item.updatedValue = item.value;
 		}
 	}
-	
+
 	public static void pushNewValue() {
 		for (PrefEntryInt item:cache) {
 			item.value = item.updatedValue;
 		}
 	}
-	
+
+	public void reload() {
+		this.value = BookmarkTreeContext.settings.getInt(name, defValue);
+	}
+
 }
--- BookmarkTree/src/com/dynamicg/bookmarkTree/prefs/PreferencesWrapper.java
+++ BookmarkTree/src/com/dynamicg/bookmarkTree/prefs/PreferencesWrapper.java
@@ -4,20 +4,21 @@ import android.graphics.Color;
 import android.text.format.Time;
 
 import com.dynamicg.bookmarkTree.BookmarkTreeContext;
+import com.dynamicg.bookmarkTree.util.BitmapScaleManager;
 
 public class PreferencesWrapper {
-	
+
 	/*
 	 * GLOBALS
 	 */
 	public static final int SORT_ALPHA = 0;
 	public static final int SORT_FOLDERS_FIRST = 1;
 	public static final int SORT_BOOKMARKS_FIRST = 2;
-	
+
 	public static final int LIST_SIZE_LARGE = 0;
 	public static final int LIST_SIZE_MEDIUM = 1;
 	public static final int LIST_SIZE_SMALL = 2;
-	
+
 	public static final int ICON_SCALING_NONE = 0;
 	public static final int ICON_SCALING_160 = 1;
 	public static final int ICON_SCALING_160_120 = 2;
@@ -25,16 +26,16 @@ public class PreferencesWrapper {
 	public static final int ICON_SCALING_240_160 = 4;
 	public static final int ICON_SCALING_160_100 = 5;
 	public static final int ICON_SCALING_160_80 = 6;
-	
+
 	/*
 	 * KEYS
 	 */
 	protected static final String KEY_FOLDER_SEPARATOR = "separator";
 	protected static final String DEFVALUE_FOLDER_SEPARATOR = "-";
-	
+
 	public static final String KEY_DISCLAIMER = "disclaimerLastDisplayed";
 	private static final String KEY_DATE_INITIALISED = "dateInitialised";
-	
+
 	/*
 	 * PREFERENCE ITEMS
 	 */
@@ -46,17 +47,21 @@ public class PreferencesWrapper {
 	public static final PrefEntryInt colorFolder = new PrefEntryInt("color.folder", Color.WHITE);
 	public static final PrefEntryInt colorBookmarkTitle = new PrefEntryInt("color.bookmarkTitle", Color.WHITE);
 	public static final PrefEntryInt colorBookmarkUrl = new PrefEntryInt("color.bookmarkUrl", Color.WHITE);
-	
+
 	public static final SeparatorPreference separatorPreference = new SeparatorPreference();
 	static {
-		String separator = BookmarkTreeContext.settings.getString(KEY_FOLDER_SEPARATOR, DEFVALUE_FOLDER_SEPARATOR);
-		PreferencesUpdater.setFolderSeparator(separator);
+		propagateSeparator();
 	}
-	
+
 	// removed from screen - is now always on:
 	//public static final PrefEntryInt keepState = new PrefEntryInt("keepState", 1);
 	public static final boolean isKeepState = true;
-	
+
+	private static void propagateSeparator() {
+		String separator = BookmarkTreeContext.settings.getString(KEY_FOLDER_SEPARATOR, DEFVALUE_FOLDER_SEPARATOR);
+		PreferencesUpdater.setFolderSeparator(separator);
+	}
+
 	/*
 	 * WRAPPERS
 	 */
@@ -66,25 +71,33 @@ public class PreferencesWrapper {
 	public static boolean isListStyleSmall() {
 		return listStyle.value == LIST_SIZE_SMALL;
 	}
-	
+
 	/*
 	 * extra handling on first call (i.e. after installation)
 	 */
 	public static void initialSetup() {
-		
+
 		if (BookmarkTreeContext.settings.contains(KEY_DISCLAIMER)) {
 			// if this key is available, the disclaimer has already been shown
 			return;
 		}
-		
+
 		PreferencesUpdater.updateAndWrite(listStyle, LIST_SIZE_MEDIUM); // as of 1.12, default list style is "medium"
-		
+
 		// "init date" value
 		Time t = new Time();
 		t.setToNow();
 		int value = Integer.parseInt(t.format("%Y%m%d"));
 		PreferencesUpdater.writeIntPref(KEY_DATE_INITIALISED, value);
-		
+
 	}
-	
+
+	public static void afterRestore() {
+		for (PrefEntryInt entry:PrefEntryInt.cache) {
+			entry.reload();
+		}
+		propagateSeparator();
+		BitmapScaleManager.init();
+	}
+
 }
