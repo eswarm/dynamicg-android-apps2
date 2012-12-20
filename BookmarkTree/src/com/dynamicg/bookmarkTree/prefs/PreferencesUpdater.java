3258868b9a8226744cd2e1ae96b2b62f2e9d9e8e
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2012-12-20 16:48:01 +0000

    add com.dynamicg.bookmarkTree.prefs.PreferencesUpdater.deletePref(String)

--- BookmarkTree/src/com/dynamicg/bookmarkTree/prefs/PreferencesUpdater.java
+++ BookmarkTree/src/com/dynamicg/bookmarkTree/prefs/PreferencesUpdater.java
@@ -9,16 +9,16 @@ import com.dynamicg.common.Logger;
 public class PreferencesUpdater {
 
 	private static final Logger log = new Logger(PreferencesUpdater.class);
-	
+
 	public static void writeAll() {
 		SharedPreferences.Editor editor = BookmarkTreeContext.settings.edit();
-		
+
 		// separator handled separately
 		if (log.debugEnabled) {
 			log.debug("write prefs - folderSeparator", PreferencesWrapper.separatorPreference.folderSeparator);
 		}
 		editor.putString ( PreferencesWrapper.KEY_FOLDER_SEPARATOR, PreferencesWrapper.separatorPreference.folderSeparator);
-		
+
 		// all the other "int" items
 		PrefEntryInt.pushNewValue();
 		for (PrefEntryInt item:PrefEntryInt.cache) {
@@ -27,15 +27,15 @@ public class PreferencesUpdater {
 				log.debug("PrefEntryInt", item.name, item.value);
 			}
 		}
-		
+
 		editor.commit();
 	}
-	
+
 	public static void setFolderSeparator(String folderSeparator) {
 		PreferencesWrapper.separatorPreference.folderSeparator = folderSeparator;
 		PreferencesWrapper.separatorPreference.nodeConcatenation = " "+folderSeparator+" ";
 	}
-	
+
 	public static void setNewSeparator(String newSeparator) {
 		setFolderSeparator(newSeparator.trim());
 	}
@@ -48,7 +48,7 @@ public class PreferencesUpdater {
 		edit.putInt(key, value);
 		edit.commit();
 	}
-	
+
 	public static void updateAndWrite(PrefEntryInt item, int newValue) {
 		item.value = newValue;
 		writeIntPref(item.name, item.value);
@@ -56,5 +56,11 @@ public class PreferencesUpdater {
 	public static void write(PrefEntryInt item) {
 		writeIntPref(item.name, item.value);
 	}
-	
+
+	public static void deletePref(String key) {
+		Editor edit = BookmarkTreeContext.settings.edit();
+		edit.remove(key);
+		edit.commit();
+	}
+
 }
