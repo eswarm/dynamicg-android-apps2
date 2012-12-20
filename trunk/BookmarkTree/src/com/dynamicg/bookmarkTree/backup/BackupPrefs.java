e13d7c1a6c48cfdb42012468ee42ea6fdc1183e0
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2012-12-20 16:41:22 +0000

    prevent initial firing of backup after fresh install

--- trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/backup/BackupPrefs.java
+++ trunk/BookmarkTree/src/com/dynamicg/bookmarkTree/backup/BackupPrefs.java
@@ -9,29 +9,29 @@ import com.dynamicg.common.Logger;
 public class BackupPrefs {
 
 	private static final Logger log = new Logger(BackupPrefs.class);
-	
+
 	public static final int BCK_OFF = 0;
 	public static final int BCK_5 = 3;
 	public static final int BCK_10 = 2;
 	public static final int BCK_20 = 1;
-	
+
 	private static final String KEY_LAST_BACKUP = "backup.last";
-    private static final String KEY_AUTO_BACKUP = "backup.auto";
-	
+	private static final String KEY_AUTO_BACKUP = "backup.auto";
+
 	private static final SharedPreferences settings = BookmarkTreeContext.settings;
-	
+
 	public static void onStartup(BookmarkTreeContext ctx) {
 		if (getAutoPrefValue()>0) {
 			checkPeriodicBackup(ctx);
 		}
 	}
-	
+
 	private static int getDayNr() {
 		long now = System.currentTimeMillis();
 		long miliPerDay = 60l * 60l * 24l * 1000l;
 		return (int) (now/miliPerDay);
 	}
-	
+
 	private static int getBackupDaysInterval() {
 		final int autoBackupValue = getAutoPrefValue();
 		switch (autoBackupValue) {
@@ -41,7 +41,7 @@ public class BackupPrefs {
 		default: return 0;
 		}
 	}
-	
+
 	private static void checkPeriodicBackup(BookmarkTreeContext ctx) {
 		int daynr = getDayNr();
 		int lastBackup = settings.getInt(KEY_LAST_BACKUP, 0);
@@ -54,23 +54,27 @@ public class BackupPrefs {
 			startBackup(ctx);
 		}
 	}
-	
+
 	public static void registerBackup() {
 		writePref(KEY_LAST_BACKUP, getDayNr());
 	}
-	
+
 	private static void startBackup(BookmarkTreeContext ctx) {
 		BackupManager.createBackup(ctx, null); // no callback
 	}
-	
+
 	private static void writePref(String key, int value) {
 		PreferencesUpdater.writeIntPref(key, value);
 	}
 
 	public static int getAutoPrefValue() {
+		if (!BookmarkTreeContext.settings.contains(KEY_LAST_BACKUP)) {
+			// prevent initial firing of backup
+			writePref(KEY_LAST_BACKUP, getDayNr());
+		}
 		return BookmarkTreeContext.settings.getInt(KEY_AUTO_BACKUP, BCK_20);
 	}
-	
+
 	public static void writeBackupPref(int newPrefValue) {
 		writePref(KEY_AUTO_BACKUP, newPrefValue);
 	}
