4ca232630c5e0141ef2f4aa4880de5eabe2f4029
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2013-02-25 17:16:42 +0000

    use android.widget.TabHost

--- branches/HomeButtonLauncher.1.6.1/src/com/dynamicg/homebuttonlauncher/MainTabHelper.java
+++ branches/HomeButtonLauncher.1.6.1/src/com/dynamicg/homebuttonlauncher/MainTabHelper.java
@@ -3,27 +3,31 @@ package com.dynamicg.homebuttonlauncher;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Typeface;
 import android.text.InputType;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TabWidget;
 import android.widget.TextView;
 
+import com.dynamicg.common.Logger;
 import com.dynamicg.homebuttonlauncher.preferences.PreferencesManager;
 
 public class MainTabHelper {
+
+	private static final Logger log = new Logger(MainTabHelper.class);
 
 	private final Context context;
 	private final MainActivityHome activity;
 	private final PreferencesManager preferences;
 
 	private int numTabs;
-	private TextView[] tabs;
+	private TabSpec[] tabs;
+	private View[] tabviews;
 
 	public MainTabHelper(MainActivityHome activity, PreferencesManager preferences) {
 		this.activity = activity;
@@ -33,61 +37,73 @@ public class MainTabHelper {
 	}
 
 	public void bindTabs() {
-		final int selectedTabIndex = preferences.getTabIndex();
+		final int selectedIndex = preferences.getTabIndex();
 		final LayoutInflater inflater = activity.getLayoutInflater();
-		final ViewGroup container = (ViewGroup)inflater.inflate(R.layout.tabs_container, null);
+		final TabHost tabhost = (TabHost)inflater.inflate(R.layout.tabs_container, null);
+		tabhost.setup();
 
-		View.OnClickListener clickListener = new OnClickListenerWrapper() {
+		final TabHost.TabContentFactory factory = new TabHost.TabContentFactory() {
 			@Override
-			public void onClickImpl(View v) {
-				int tabIndex = (Integer)v.getTag();
-				decorateTabs(tabIndex);
-				activity.updateOnTabSwitch(tabIndex);
+			public View createTabContent(String tag) {
+				return new View(context);
 			}
 		};
+
+		this.tabs = new TabSpec[numTabs];
+		for (int i=0;i<numTabs;i++) {
+			TabSpec spec = tabhost.newTabSpec(Integer.toString(i));
+			spec.setIndicator(preferences.getTabTitle(i));
+			spec.setContent(factory);
+			tabhost.addTab(spec);
+			tabs[i] = spec;
+		}
+
+		tabhost.setCurrentTab(selectedIndex);
+
+		tabhost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+			@Override
+			public void onTabChanged(String tabId) {
+				int index = Integer.parseInt(tabId);
+				activity.updateOnTabSwitch(index);
+			};
+		});
 
 		View.OnLongClickListener longClickListener = new OnLongClickListenerWrapper() {
 			@Override
 			public boolean onLongClickImpl(View v) {
-				int tabIndex = (Integer)v.getTag();
-				editLabel((TextView)v, tabIndex);
+				int index = (Integer)v.getTag(R.id.buttonOk);
+				editLabel(index);
 				return true;
 			}
 		};
 
-		int height = (int)context.getResources().getDimension(R.dimen.tabHeight);
-		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.MATCH_PARENT, height, 1f/numTabs);
-		tabs = new TextView[numTabs];
+		int tabHeight = (int)context.getResources().getDimension(R.dimen.tabHeight);
+
+		TabWidget tabWidget = tabhost.getTabWidget();
+		if (log.isDebugEnabled) {
+			log.debug("tabWidget", tabWidget.getChildCount(), tabWidget.getChildAt(0));
+		}
+		this.tabviews = new View[numTabs];
 		for (int i=0;i<numTabs;i++) {
-			// for 1px margin between tabs:
-			//			if (i>0) {
-			//				lp.setMargins(0, 0, 1, 0);
-			//			}
-			TextView node = new TextView(context);
-			node.setLayoutParams(lp);
-			node.setOnClickListener(clickListener);
+			View node = tabWidget.getChildAt(i);
+			node.setTag(R.id.buttonOk, i);
 			node.setLongClickable(true);
 			node.setOnLongClickListener(longClickListener);
-			node.setSingleLine();
-			node.setTag(i);
-			setLabel(node, preferences.getTabTitle(i));
-			container.addView(node);
-			// done:
-			tabs[i] = node;
+			// same as time recording, see com.dynamicg.timerecording.util.ui.TabHostUtil.prepareTabs(Dialog, int, int[], int[])
+			node.getLayoutParams().height = tabHeight;
+			node.setPadding(0, node.getPaddingTop(), 0, node.getPaddingBottom());
+			tabviews[i] = node;
 		}
-
-		decorateTabs(getValidatedTabNum(selectedTabIndex, numTabs));
 
 		// attach after header
 		View header = activity.findViewById(R.id.headerContainer);
 		ViewGroup main = (ViewGroup)header.getParent();
-		main.addView(container, main.indexOfChild(header)+1);
+		main.addView(tabhost, main.indexOfChild(header)+1);
 
 	}
 
-	protected void editLabel(final TextView labelNode, final int tabIndex) {
-		final String currentLabel = preferences.getTabTitle(tabIndex);
+	protected void editLabel(final int index) {
+		final String currentLabel = preferences.getTabTitle(index);
 
 		final EditText editor = new EditText(context);
 		editor.setText(currentLabel);
@@ -95,7 +111,7 @@ public class MainTabHelper {
 		if (currentLabel.length()>0) {
 			editor.setSelection(currentLabel.length());
 		}
-		editor.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+		editor.setInputType(InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		DialogInterface.OnClickListener okListener = new OnClickListenerDialogWrapper(context) {
@@ -103,8 +119,8 @@ public class MainTabHelper {
 			public void onClickImpl(DialogInterface dialog, int which) {
 				String newLabel = editor.getText().toString();
 				newLabel = newLabel!=null?newLabel:"";
-				setLabel(labelNode, newLabel);
-				preferences.writeTabTitle(tabIndex, newLabel);
+				setLabel(index, newLabel);
+				preferences.writeTabTitle(index, newLabel);
 			}
 		};
 		builder.setPositiveButton(R.string.buttonOk, okListener );
@@ -116,27 +132,19 @@ public class MainTabHelper {
 		dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 	}
 
-	private void setLabel(TextView tab, String label) {
-		tab.setTextSize(12);
-		tab.setGravity(Gravity.CENTER);
-		tab.setText(label);
-		tab.setTypeface(Typeface.DEFAULT_BOLD, Typeface.BOLD);
+	private void setLabel(int index, String label) {
+		log.debug("set label", label, index);
+		//tabs[index].setIndicator(label); // does not work
+		TextView title = (TextView)tabviews[index].findViewById(android.R.id.title);
+		title.setText(label);
 	}
 
 	public void redraw() {
-		View tabContainer = activity.findViewById(R.id.mainTabContainer);
+		View tabContainer = activity.findViewById(android.R.id.tabhost);
 		if (tabContainer!=null) {
 			((ViewGroup)tabContainer.getParent()).removeView(tabContainer);
 		}
 		bindTabs();
-	}
-
-	private void decorateTabs(int selectedTab) {
-		for (int i=0;i<numTabs;i++) {
-			View tab = tabs[i];
-			int bgres = i==selectedTab ? R.drawable.tab_active_shape : R.drawable.tab_inactive_selector;
-			tab.setBackgroundResource(bgres);
-		}
 	}
 
 	public static int getValidatedTabNum(int selectedTabIndex, int numTabs) {
