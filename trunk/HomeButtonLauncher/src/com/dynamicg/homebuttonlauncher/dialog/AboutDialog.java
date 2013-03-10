478eab9be0870ed82b6aa819ffa4bf5bf9cd0421
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2013-03-10 10:33:42 +0000

    add "translation request" to About

--- trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/dialog/AboutDialog.java
+++ trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/dialog/AboutDialog.java
@@ -1,8 +1,12 @@
 package com.dynamicg.homebuttonlauncher.dialog;
 
+import java.util.HashMap;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
@@ -10,6 +14,7 @@ import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.TextView;
 
+import com.dynamicg.common.Logger;
 import com.dynamicg.common.MarketLinkHelper;
 import com.dynamicg.common.SystemUtil;
 import com.dynamicg.homebuttonlauncher.OnClickListenerWrapper;
@@ -18,9 +23,21 @@ import com.dynamicg.homebuttonlauncher.tools.DialogHelper;
 
 public class AboutDialog extends Dialog {
 
-	private final String REPOSITORY = "https://dynamicg-android-apps2.googlecode.com/svn/trunk/HomeButtonLauncher";
+	private static final Logger log = new Logger(AboutDialog.class);
 
-	final Context context;
+	private static final String REPOSITORY = "https://dynamicg-android-apps2.googlecode.com/svn/trunk/HomeButtonLauncher";
+
+	private final Context context;
+
+	private final HashMap<String, String> pendingTranslations = new HashMap<String, String>();
+	{
+		pendingTranslations.put("ko", "Korean");
+		pendingTranslations.put("ja", "Japanese");
+		pendingTranslations.put("tr", "Turkish");
+		pendingTranslations.put("ru", "Russian");
+		pendingTranslations.put("ar", "Arabic");
+		pendingTranslations.put("es", "Spanish");
+	}
 
 	public AboutDialog(Activity activity) {
 		super(activity);
@@ -60,6 +77,28 @@ public class AboutDialog extends Dialog {
 		creditsLabel.setSpan(new UnderlineSpan(), 0, creditsLabel.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 		setLine(R.id.aboutCredits, creditsLabel);
 
+		setupTranslationRequest();
+	}
+
+	private void setupTranslationRequest() {
+		final TextView node = (TextView)findViewById(R.id.aboutTranslationRequest);
+		final String languageCd = log.isDebugEnabled ? "ko" : Locale.getDefault().getLanguage();
+		if (pendingTranslations.containsKey(languageCd)) {
+			String label1 = "Can you help with "+pendingTranslations.get(languageCd)+" translation? ";
+			String label2 = "Please contact developer";
+			SpannableString str = new SpannableString(label1+label2);
+			underline(str, label1.length(), label1.length()+label2.length());
+			node.setText(str);
+			node.setOnClickListener(new OnClickListenerWrapper() {
+				@Override
+				public void onClickImpl(View view) {
+					composeEmail();
+				}
+			});
+		}
+		else {
+			node.setVisibility(View.GONE);
+		}
 	}
 
 	private void setLine(int id, CharSequence str) {
@@ -68,11 +107,22 @@ public class AboutDialog extends Dialog {
 
 	private void setRateLabel(TextView node) {
 		final String label = "\u21d2 "+context.getString(R.string.aboutPleaseRate)+" \u21d0";
-		final int underlineFrom = 2;
-		final int underlineTo = label.length()-2;
 		SpannableString str = new SpannableString(label);
+		underline(str, 2, label.length()-2);
+		node.setText(str, TextView.BufferType.SPANNABLE);
+	}
+
+	private void underline(SpannableString str, int underlineFrom, int underlineTo) {
 		str.setSpan(new UnderlineSpan(), underlineFrom, underlineTo, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-		node.setText(str,TextView.BufferType.SPANNABLE);
+	}
+
+	private void composeEmail() {
+		String label = "Home Button Launcher ("+Locale.getDefault().getLanguage()+")";
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("message/rfc822");
+		intent.putExtra(Intent.EXTRA_EMAIL, new String[]{SystemUtil.AUTHOR});
+		intent.putExtra(Intent.EXTRA_SUBJECT, label);
+		context.startActivity(Intent.createChooser(intent, "Send Email"));
 	}
 
 }
