c5ef2998d866034aa25f3b3129304bcdef1f190f
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2013-03-21 21:19:52 +0000

    icons backup/restore

--- trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/tools/drive/XmlReader.java
+++ trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/tools/drive/XmlReader.java
@@ -11,9 +11,16 @@ import java.util.zip.GZIPInputStream;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import android.content.Context;
 import android.util.Xml;
 
+import com.dynamicg.common.Logger;
+import com.dynamicg.common.SystemUtil;
+import com.dynamicg.homebuttonlauncher.tools.icons.ShortcutHelper;
+
 public class XmlReader {
+
+	private static final Logger log = new Logger(XmlReader.class);
 
 	private final XmlPullParser parser;
 	private int evtype;
@@ -29,7 +36,7 @@ public class XmlReader {
 		evtype = parser.getEventType();
 	}
 
-	public List<Map<String, String>> getContent()throws Exception {
+	public List<Map<String, String>> getContent(Context context) throws Exception {
 		final List<Map<String, String>> content = new ArrayList<Map<String,String>>();
 		Map<String, String> map = null;
 		while(true) {
@@ -49,6 +56,26 @@ public class XmlReader {
 				else {
 					nextItem();
 					map.put(tag, parser.getText());
+				}
+			}
+
+			boolean iconRecovery = evtype==XmlPullParser.END_TAG
+					&& XmlGlobals.TAG_ENTRY.equals(parser.getName())
+					&& map!=null
+					&& map.containsKey(XmlGlobals.ENTRY_ICON_DATA)
+					;
+			if (iconRecovery) {
+				// direct icon recovery. we don't pass this back as "content" entry
+				try {
+					ShortcutHelper.initIconDir(context);
+					String entryKey = map.get(XmlGlobals.ENTRY_KEY);
+					String entryIconData = map.get(XmlGlobals.ENTRY_ICON_DATA);
+					ShortcutHelper.restoreIcon(entryKey, entryIconData);
+					boolean remove = content.remove(map); // NO NEED TO PASS THIS BACK
+					log.debug("icon recovery done", entryKey, remove);
+				}
+				catch (Throwable t) {
+					SystemUtil.dumpIfDevelopment(t);
 				}
 			}
 		}
