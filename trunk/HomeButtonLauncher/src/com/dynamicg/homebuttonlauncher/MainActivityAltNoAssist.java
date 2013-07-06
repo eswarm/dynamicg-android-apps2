3ae81920fd750bd756c2a1bb44a4b9bb40c71ebb
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2013-07-06 17:58:05 +0000

    release 2.5 - temporary workaround for samsung "recent apps" crash

---
+++ trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/MainActivityAltNoAssist.java
@@ -0,0 +1,8 @@
+package com.dynamicg.homebuttonlauncher;
+
+/*
+ * alternate assist activity without assist icon
+ * see http://developer.samsung.com/forum/board/thread/view.do?boardName=GeneralB&messageId=238854&startId=zzzzz~
+ */
+public class MainActivityAltNoAssist extends MainActivityHome {
+}
