dcc9bc20ceb9c23f5126d00ab7831fc92b91d7c6
dynamicg.android@gmail.com <dynamicg.android@gmail.com@d2041400-0d5f-ab11-4b27-9536f9335476>
2013-12-04 18:25:05 +0000

    catch "android.intent.action.CALL_PRIVILEGED"

--- trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/tools/DirectDialWrapper.java
+++ trunk/HomeButtonLauncher/src/com/dynamicg/homebuttonlauncher/tools/DirectDialWrapper.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 
 public class DirectDialWrapper {
 
+	private static final String CALL_DFLT = Intent.ACTION_CALL; // "android.intent.action.CALL"
 	private static final String CALL_PRIVILEGED = "android.intent.action.CALL_PRIVILEGED";
 
 	public static void updateIntent(Intent intent) {
@@ -13,10 +14,7 @@ public class DirectDialWrapper {
 
 		// action_call requires android.permission.CALL_PHONE so we change it to "dial" (which opens the dial pad with the given phone number)
 		String action = intent!=null ? intent.getAction() : null;
-		if (Intent.ACTION_CALL.equals(action) // "android.intent.action.CALL"
-				|| CALL_PRIVILEGED.equals(action)
-				)
-		{
+		if (CALL_DFLT.equals(action) || CALL_PRIVILEGED.equals(action)) {
 			intent.setAction(Intent.ACTION_DIAL);
 		}
 
